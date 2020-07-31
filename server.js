61524f77e6036f065c4001ee5c8987635b97816d
Mont0360 <mont0360@umn.edu>
2020-07-30 19:11:58 -0500

    updated file setup

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+// const exerciseRoutes = require("./routes/exercises");
+// const workoutRoutes = require("./routes/workouts");
+const apiRoutes = require("./routes/api");
+const viewRoutes = require("./routes/views");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+app.use(apiRoutes);
+app.use(viewRoutes);
+// app.use(workoutRoutes);
+
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.listen(PORT, () => console.log("listening on port: ", PORT));
